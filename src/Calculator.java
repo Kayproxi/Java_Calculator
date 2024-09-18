6e4352d8a418aafc209b78d9c32ae0671600a225
adikajanai <adikajanai@gmail.com>
2024-09-18 12:41:44 +0000

    getting started with the calculator app. Created an instance and a constructor.

---
+++ src/Calculator.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+java.awt.event.*;
+
+public class Calculator implements ActionListener {
+    JFrame frame;
+    JButton numberButtons;
+     Calculator(){
+
+     }
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
