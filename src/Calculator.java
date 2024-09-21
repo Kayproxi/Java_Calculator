22edc3a3ddcf2f28ca53b0e4b73162409aa4310f
adikajanai <adikajanai@gmail.com>
2024-09-21 21:44:41 +0000

    added functionality to function buttons. added a negative button and enforced functionality

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,9 +8,9 @@ public class Calculator implements ActionListener {
     JFrame frame;
     JTextField field;
     JButton[] numberButtons = new JButton[10];
-    JButton[] functionButtons = new JButton[8];
+    JButton[] functionButtons = new JButton[9];
     JButton addButton, subButton, mulButton, divButton;
-    JButton decButton, delButton, clrButton, equButton;
+    JButton decButton, delButton, clrButton, equButton, negButton;
     JPanel panel;
     private Object font;
     Font myfont = new Font("Gigi",Font.BOLD, 30);
@@ -35,6 +35,7 @@ public class Calculator implements ActionListener {
           delButton = new JButton("del");
           equButton = new JButton("=");
           divButton = new JButton("/");
+          negButton = new JButton("(-)");
 
           functionButtons[0] = addButton;
           functionButtons[1] = subButton;
@@ -44,8 +45,10 @@ public class Calculator implements ActionListener {
           functionButtons[5] = clrButton;
           functionButtons[6] = decButton;
           functionButtons[7] = equButton;
+          functionButtons[8] = negButton;
 
-          for( int i=0; i<8; i++){
+
+          for( int i=0; i<9; i++){
               functionButtons[i].addActionListener(this);
               functionButtons[i].setFont(myfont);
               functionButtons[i].setFocusable(false);
@@ -58,8 +61,9 @@ public class Calculator implements ActionListener {
              numberButtons[i].setFocusable(false);
          }
 
-         delButton.setBounds(50,430,145,50);
-         clrButton.setBounds(205,430,145,50);
+         delButton.setBounds(150,430,100,50);
+         clrButton.setBounds(250,430,100,50);
+         negButton.setBounds(50,430,100,50);
 
          panel = new JPanel();
          panel.setBounds(50,100,300,300);
@@ -87,9 +91,11 @@ public class Calculator implements ActionListener {
 
 
 
+
           frame.add(panel);
           frame.add(delButton);
           frame.add(clrButton);
+          frame.add(negButton);
           frame.add(field);
           frame.setVisible(true);
 
@@ -101,10 +107,70 @@ public class Calculator implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-         for(int i=0; i<10; i++)
+         for(int i=0; i<10; i++){
              if(e.getSource()==numberButtons[i]){
                  field.setText(field.getText().concat(String.valueOf(i)));
              }
+         }
+         if (e.getSource() == decButton){
+             field.setText(field.getText().concat("."));
+         }
+        if (e.getSource() == addButton){
+            num1 = Double.parseDouble(field.getText());
+            operator = '+';
+            field.setText("");
+        }
+        if (e.getSource() == subButton){
+            num1 = Double.parseDouble(field.getText());
+            operator = '-';
+            field.setText("");
+        }
+        if (e.getSource() == divButton){
+            num1 = Double.parseDouble(field.getText());
+            operator = '/';
+            field.setText("");
+        }
+        if (e.getSource() == mulButton){
+            num1 = Double.parseDouble(field.getText());
+            operator = 'x';
+            field.setText("");
+        }
+        if(e.getSource() == equButton){
+            num2 = Double.parseDouble(field.getText());
+
+            switch(operator){
+                case '+':
+                    result= num1 + num2;
+                    break;
+                case '-':
+                    result= num1 - num2;
+                    break;
+                case '/':
+                    result= num1 / num2;
+                    break;
+                case 'x':
+                    result= num1 * num2;
+                    break;
+            }
+            field.setText(String.valueOf(result));
+            num1=result;
+        }
+        if (e.getSource() == clrButton){
+           field.setText("");
+        }
+        if (e.getSource() == delButton){
+            String string = field.getText();
+            field.setText("");
+            for(int i = 0; i< string.length()-2; i++){
+                field.setText(field.getText()+string.charAt(i));
+            }
+        }
+        if (e.getSource() == negButton){
+            double temp = Double.parseDouble(field.getText());
+            temp *= -1;
+            field.setText(String.valueOf(temp));
+        }
+
 
     }
 }
